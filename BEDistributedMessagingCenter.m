e3297d91629cea20a8a693e0ca30d762996c0fb9
Ryan Petrich <rpetrich@gmail.com>
2014-05-23 11:50:25 -0600

    Greatly simplify block storage and dispatch

--- BEDistributedMessagingCenter.m
+++ BEDistributedMessagingCenter.m
@@ -14,98 +14,19 @@
 #import <objc/runtime.h>
 #import "BEDistributedMessagingCenter.h"
 
-//Private wrapper class to store the blocks, since CPDistributedMessagingCenter can only callback to methods.
-@interface BEBlockMapper : NSObject
-+ (instancetype)sharedInstance;
-- (void *)addBlock:(BEDMCAnswerBlock)block;
-@end
-
-@implementation BEBlockMapper {
-    NSMutableDictionary *_callbacks;
-    NSUInteger _key;
-    OSSpinLock _spinLock;
-}
-
-+ (instancetype)sharedInstance {
-    static BEBlockMapper* mapperInstance;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        mapperInstance = [[self alloc] init];
-    });
-    return mapperInstance;
-}
-
-- (instancetype)init {
-    if ((self = [super init])) {
-        _callbacks = [[NSMutableDictionary alloc] init];
-    }
-    return self;
-}
-
-/**
- @private 
- generates a new contextID
- @return context ID.
- */
-- (NSUInteger)nextContextID {
-    NSUInteger nextKey = _key++;
-    while (_callbacks[@(nextKey)]) {
-        nextKey = _key++;
-    }
-    return nextKey;
-}
-
-/**
- Store a new block for dispatching, and return its context ID
- Example usage:
- @code
- static BEBlockMapper* mapper = nil;
- ...
- if (!mapper) {
- static dispatch_once_t onceToken;
- dispatch_once(&onceToken, ^{
- mapper = [[BEBlockMapper alloc] init];
- });
- }
- ...
- [mapper addBlock:someBlock];
- @endcode
- @param BEDMCAnswerBlock block - the block to be added to the mapper.
- @return context ID to register/call the block on the mapper.
- */
-- (void *)addBlock:(BEDMCAnswerBlock)block {
-    OSSpinLockLock(&_spinLock);
-    NSUInteger contextId = [self nextContextID];
-    _callbacks[@(contextId)] = block;
-    OSSpinLockUnlock(&_spinLock);
-    return (void *)contextId;
-}
-
-/**
- @private method to handle the calling of the passed blocks
- */
-- (void)messagingCenter:(CPDistributedMessagingCenter *)messagingCenter gotReply:(id)reply unknown:(void *)unknown context:(void *)context {
-    if (![messagingCenter isKindOfClass:[BEDistributedMessagingCenter class]]) {
-        NSLog(@"BEDistributedMessagingCenter: Unexpected type of messagingCenter: %@", [messagingCenter class]);
-    }
-    NSUInteger contextId = (NSUInteger)context;
-    NSNumber *key = @(contextId);
-    OSSpinLockLock(&_spinLock);
-    BEDMCAnswerBlock callback = _callbacks[key];
-    [_callbacks removeObjectForKey:key];
-    OSSpinLockUnlock(&_spinLock);
-    if (callback) {
-        callback(reply);
-    }
-}
-
-@end
-
 @implementation BEDistributedMessagingCenter
 
 + (instancetype)centerNamed:(NSString *)centerName {
     return (BEDistributedMessagingCenter*)[super centerNamed:centerName];
 }
+
+- (void)messagingCenter:(CPDistributedMessagingCenter *)messagingCenter gotReply:(id)reply unknown:(void *)unknown context:(void *)context {
+    if (context) {
+        BEDMCAnswerBlock callback = (__bridge_transfer BEDMCAnswerBlock)context;
+        callback(reply);
+    }
+}
+
 
 /**
  Send a message with given name and userInfo-data and calls the passed block when a reply is received.
@@ -121,7 +42,7 @@
  @param BEDMCAnswerBlock block - the block to be executed on reply.
  */
 - (void)sendMessageAndReceiveReplyName:(NSString*)messageName userInfo:(id)userInfo toCallbackBlock:(BEDMCAnswerBlock)block {
-    [self sendMessageAndReceiveReplyName:messageName userInfo:userInfo toTarget:[BEBlockMapper sharedInstance] selector:@selector(messagingCenter:gotReply:unknown:context:) context:[[BEBlockMapper sharedInstance] addBlock:block]];
+    [self sendMessageAndReceiveReplyName:messageName userInfo:userInfo toTarget:self selector:@selector(messagingCenter:gotReply:unknown:context:) context:(__bridge_retained void *)[block copy]];
 }
 
 @end
